402fa71bc74125258f6ec7796c68e80e6259d466
bakhtiarabid <abid.bu16@gmail.com>
2022-08-29 21:58:10 +0600

    NavBar Section Added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import PrivateRoute from "./Login/PrivateRoute/PrivateRoute";
+import AuthProvider from './contexts/AuthProvider';
 import Home from './Pages/Home/Home/Home';
+
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+     <div className="App">
+        <AuthProvider>
+           <Router>
+              <Switch>
+                 <Route exact path="/">
+                    <Home />
+                 </Route>
+                 <Route path="/home">
+                    <Home />
+                 </Route>
+
+                 <Route path="/login">{/* <Login></Login> */}</Route>
+
+                 <PrivateRoute path="/myplans">
+                    {/* <MyPlans></MyPlans> */}
+                 </PrivateRoute>
+
+                 <PrivateRoute path="/manageplans/">
+                    {/* <ManagePlans></ManagePlans> */}
+                 </PrivateRoute>
+                 <PrivateRoute path="/addplan">
+                    {/* <AddPlan></AddPlan> */}
+                 </PrivateRoute>
+                 <PrivateRoute path="/plan/:planId">
+                    {/* <SinglePlanDetail></SinglePlanDetail> */}
+                 </PrivateRoute>
+                 <Route path="*">{/* <NotFound></NotFound> */}</Route>
+              </Switch>
+              {/* <Footer></Footer> */}
+           </Router>
+        </AuthProvider>
+     </div>
   );
 }
 
--- src/Pages/Shared/Navigation.js
+++ src/Pages/Shared/Navigation.js
@@ -1,10 +1,127 @@
 import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Button, Container, Form, Nav, Navbar } from 'react-bootstrap';
+import logo from "../../images/logo.png"
+import { Link } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
+import useAuth from './../../hooks/useAuth';
 
 const Navigation = () => {
+    const { user, logOut } = useAuth();
     return (
-    <h1>Navigation</h1>
+       <div>
+          <Navbar
+             className=""
+             style={{
+                backgroundColor: "#241f1f",
+             }}
+             //  bg="dark"
+             variant="dark"
+             sticky="top"
+             collapseOnSelect
+             expand="lg"
+          >
+             <Container>
+                <Navbar.Brand href="/home">
+                   <img
+                      src={logo}
+                      width="100"
+                      height="100"
+                      className="d-inline-block align-top"
+                      alt="React Bootstrap logo"
+                   />{" "}
+                   <div className="d-inline-block pt-4">
+                      N E W B R I G H T <br />E C O M M E R C E
+                   </div>
+                </Navbar.Brand>
+                <Navbar.Toggle />
+                <Navbar.Collapse className="justify-content-end">
+                   <Nav.Link as={HashLink} to="/home" className="text-white">
+                      HOME
+                   </Nav.Link>
+                   <Nav.Link as={HashLink} to="/home" className="text-white">
+                      SHOP
+                   </Nav.Link>
+                   <Nav.Link as={HashLink} to="/home" className="text-white">
+                      EXPLORE
+                   </Nav.Link>
+                   {user?.displayName ? (
+                      <Nav.Link
+                         as={HashLink}
+                         to="/myplans"
+                         className="text-white"
+                      >
+                         My Plans
+                      </Nav.Link>
+                   ) : (
+                      ""
+                   )}
+                   {user?.displayName ? (
+                      <Nav.Link
+                         as={HashLink}
+                         to="/manageplans"
+                         className="text-white"
+                      >
+                         Manage All Plans
+                      </Nav.Link>
+                   ) : (
+                      ""
+                   )}
+                   {user?.displayName ? (
+                      <Nav.Link
+                         as={HashLink}
+                         to="/addplan"
+                         className="text-white"
+                      >
+                         Add A Plan
+                      </Nav.Link>
+                   ) : (
+                      ""
+                   )}
+
+                   {user?.displayName ? (
+                      <Button
+                         className="text-white"
+                         onClick={logOut}
+                         variant=""
+                      >
+                         Logout
+                      </Button>
+                   ) : (
+                      <div className="">
+                         <Nav.Link as={Link} to="/login" className="text-white">
+                            LOGIN/REGISTER
+                         </Nav.Link>
+                      </div>
+                   )}
+                   <Navbar.Text className="log-in">
+                      {user?.displayName ? (
+                         <span className="me-2 text-white">Signed in as:</span>
+                      ) : (
+                         ""
+                      )}
+                      <a href="#login">
+                         {user?.displayName}{" "}
+                         <img
+                            src={user?.photoURL}
+                            className="rounded-circle ms-3"
+                            style={{ width: "55px" }}
+                            alt=""
+                         />
+                      </a>
+                   </Navbar.Text>
+                   <Form className="d-flex">
+                      <Form.Control
+                         type="search"
+                         placeholder="Search"
+                         className="me-2"
+                         aria-label="Search"
+                      />
+                      <Button variant="outline-success">Search</Button>
+                   </Form>
+                </Navbar.Collapse>
+             </Container>
+          </Navbar>
+       </div>
     );
 };
 
