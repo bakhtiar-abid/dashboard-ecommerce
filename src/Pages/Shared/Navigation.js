27583884a1922ccc354ff1bc719ceeb8273928b2
bakhtiarabid <abid.bu16@gmail.com>
2022-08-29 20:39:17 +0600

    Adding Firebase Authentication For Admin and Editor

---
+++ src/Pages/Shared/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { HashLink } from "react-router-hash-link";
+
+const Navigation = () => {
+    return (
+    <h1>Navigation</h1>
+    );
+};
+
+export default Navigation;
